3315efc9e5d6335c23935fbc3d4595010956de02
tconey23 <145934889+tconey23@users.noreply.github.com>
2024-04-19 16:34:49 -0600

    switching repos / rebuilding server code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(express.json()); // This line is crucial
 const port = process.env.PORT || 3000;
 const data = require('./data');
 
