473803a1ca44d487e638cbcd210e001d15c0758a
tconey23 <145934889+tconey23@users.noreply.github.com>
2024-04-19 09:49:38 -0600

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 const port = process.env.PORT || 3000;
 const data = require('./data');
