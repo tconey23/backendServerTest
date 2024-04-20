360b738f74fa0b16e532f529088cb2178d213372
tconey23 <145934889+tconey23@users.noreply.github.com>
2024-04-20 10:08:37 -0600

    updated BE code

--- server.js
+++ server.js
@@ -91,7 +91,10 @@ app.delete('/api/v1/data/users/:id', (req, res) => {
     if (quoteIndex !== -1) {
       data.users[userIndex]["favorite quotes"].splice(quoteIndex, 1);
       console.log('Updated quotes for user:', data.users[userIndex]["favorite quotes"]);
-      res.status(200).send({ message: "Quote deleted successfully." });
+      res.status(200).json({
+        message: "Quote deleted successfully.",
+        "favorite quotes": user["favorite quotes"]
+    });
     } else {
       res.status(404).send({ message: "Quote not found." });
     }
